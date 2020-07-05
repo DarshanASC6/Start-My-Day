27c6bcdfc98fb0ee70ff732b9372e914972e935a
Darshan Singh <darshan.singh.tech@gmail.com>
2020-07-05 15:50:06 -0400

    whatever

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import subprocess
 import webbrowser
 import json
+import time
+import playsound
 
 with open('data.json') as f:
     data = json.load(f)
@@ -15,11 +17,25 @@ def web(link):
     webbrowser.open(link)
 
 
+time_count = 12
+
+
+def timer(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
+    for i in range(0, 3):
+        playsound.playsound('chime.mp3')
+    print("take a break")
+
+
 for i in data['open']['sites']:
     web(i['link'])
     # Opens up a Firefox browser window with a bunch of preset tabs
 
-
-for filepaths in data['open']['filepaths']:
-    open(filepaths['path'])
+for item in data['open']['filepaths']:
+    open(item['path'])
     # Opens up my IDE
